58f667a61adde22545772d030bbda60f92a458f9
Tatsuya Ishikawa <ishikawa@jsk.imi.i.u-tokyo.ac.jp>
2020-04-10 11:33:37 +0900

    tmp_jaxon_red_and_blue

--- hrpsys_ros_bridge_tutorials/src/hrpsys_ros_bridge_tutorials/jaxon_blue_hrpsys_config.py
+++ hrpsys_ros_bridge_tutorials/src/hrpsys_ros_bridge_tutorials/jaxon_blue_hrpsys_config.py
@@ -206,13 +206,15 @@ class JAXON_BLUEHrpsysConfigurator(URATAHrpsysConfigurator):
         stp.eefm_k2=[-0.343983,-0.343983]
         stp.eefm_k3=[-0.161465,-0.161465]
 
-        stp.swing2landing_transition_time = 0.05
-        stp.landing_phase_time = 0.1
-        stp.landing2support_transition_time = 0.5
+        # stp.swing2landing_transition_time = 0.05
+        # stp.landing_phase_time = 0.1
+        # stp.landing2support_transition_time = 0.5
 
-        stp.joint_control_mode = OpenHRP.AutoBalanceStabilizerService.JOINT_TORQUE
-        # leg_gains = {"support_pgain":[5,30,10,5,0.5,0.1], "support_dgain":[70,70,50,10,1,3], "landing_pgain":[5,30,10,1,0.5,0.1], "landing_dgain":[70,70,50,10,1,3]}
-        # arm_gains = {"support_pgain":[100,100,100,100,100,100,100], "support_dgain":[100,100,100,100,100,100,100], "landing_pgain":[100,100,100,100,100,100,100], "landing_dgain":[100,100,100,100,100,100,100]}
+        # stp.joint_control_mode = OpenHRP.AutoBalanceStabilizerService.JOINT_TORQUE
+        # leg_gains = {"support_pgain": [5, 30, 10, 5, 0.5, 0.1], "support_dgain": [70, 70, 50, 10, 1, 3],
+        #              "landing_pgain": [5, 30, 10, 1, 0.5, 0.1], "landing_dgain": [70, 70, 50, 10, 1, 3]}
+        # arm_gains = {"support_pgain": [100, 100, 100, 100, 100, 100, 100], "support_dgain": [100, 100, 100, 100, 100, 100, 100],
+        #              "landing_pgain": [100, 100, 100, 100, 100, 100, 100], "landing_dgain": [100, 100, 100, 100, 100, 100, 100]}
         # stp.joint_servo_control_parameters = map(lambda x : OpenHRP.AutoBalanceStabilizerService.JointServoControlParameter(**x), [leg_gains, leg_gains, arm_gains, arm_gains])
 
         self.setSTParameters(stp)
@@ -223,7 +225,8 @@ class JAXON_BLUEHrpsysConfigurator(URATAHrpsysConfigurator):
         self.setDefaultSTParameters()
 
     def setDefaultRHSettings(self):
-        self.setJointControlMode("all", OpenHRP.RobotHardwareService.TORQUE)
+        pass
+        # self.setJointControlMode("all", OpenHRP.RobotHardwareService.TORQUE)
 
 if __name__ == '__main__':
     hcf = JAXON_BLUEHrpsysConfigurator()
